13db43f720ba15578662b60b260b12ebbc350bfc
María Gárgoles <mariagargoles@live.com>
2024-05-24 10:19:53 +0200

    add router and pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { IndexPage } from './pages/IndexPage'
+import { MyPhotosPage } from './pages/MyPhotosPage'
+import { Provider } from "react-redux"
+import {store} from "./app/store.js"
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<IndexPage/>} />
+        <Route path="/MyPhotos" element={<MyPhotosPage />} />
+      </Routes>
+    </BrowserRouter>
+    </Provider>
+    </React.StrictMode>,
 )
