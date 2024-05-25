193809c4a758cdae1fc3abfb706d0a604e395cc4
María Gárgoles <mariagargoles@live.com>
2024-05-25 22:31:26 +0200

    add modifi slice

--- src/feature/photos/PhotoSlide.js
+++ src/feature/photos/PhotoSlide.js
@@ -8,13 +8,30 @@ export const PhotoSlice = createSlice({
     error: null,
   },
   reducers: {
-    addPhoto: (state, action) => {
+    addFavoritesPhoto: (state, action) => {
       state.data.push(action.payload);
+      localStorage.setItem("favorites", JSON.stringify(state.data));
     },
-    removePhoto: (state, action) => {
+    removeFavoritesPhoto: (state, action) => {
       state.data = state.data.filter((image) => image.id !== action.payload.id);
+      localStorage.setItem("favorites", JSON.stringify(state.data));
+    },
+    modifyPhotoDescription: (state, action) => {
+      state.data = state.data.map((image) => {
+        if (image.id === action.payload.id) {
+          image.description = action.payload.description;
+        }
+        return image;
+      });
+      localStorage.setItem("favorites", JSON.stringify(state.data));
     },
   },
 });
 
-export const { addPhoto, removePhoto } = PhotoSlice.actions;
+export const {
+  addFavoritesPhoto,
+  removeFavoritesPhoto,
+  modifyPhotoDescription,
+} = PhotoSlice.actions;
+
+export default PhotoSlice.reducer;
