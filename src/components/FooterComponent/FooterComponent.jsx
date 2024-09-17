c0432cca10d3a5bcef3aaf40ba2747e27009f1f9
MariaGargoles <mariagargoles@live.com>
2024-09-17 15:18:36 +0200

    ADD: styles to nav

--- src/components/FooterComponent/FooterComponent.jsx
+++ src/components/FooterComponent/FooterComponent.jsx
@@ -3,7 +3,7 @@ export const FooterComponent = () => {
 return (<>
     <footer className='FooterContainer'>
         <img></img>
-        <h2>All Photos Copyright</h2>
+        <h2>All Photos @Copyright</h2>
         <div className='FooterContainerimg'>
             <img className='FooterContainer__imagen1'/>
             <img className='FooterContainer__imagen2'/>
--- src/components/Navbarcomponent/NavbarComponent.jsx
+++ src/components/Navbarcomponent/NavbarComponent.jsx
@@ -1,14 +1,16 @@
 import './NavbarComponent.css'
 import React from 'react'
 import { NavLink } from 'react-router-dom';
+import logo from '../../assets/logo.png';
 
 export const NavbarComponent = () => {
 
     return (
         <nav className='NavarContainer'> 
+            
             <div className='NavarContainerimg'>
-                <img className='NavarContainer__imagen1'/>
-                <h2 className='NavarContainer__title'>All Photos</h2>
+                <img src={logo} alt="Logo" className='NavarContainer__imagen1' />
+                <h1 className='NavarContainer__MainTitle'>image galery</h1>
             </div>
             <ul className='NavarContainer__Menu'>
                 <NavLink  to="/" className='NavarContainer__Menu--item'>Home</NavLink >
