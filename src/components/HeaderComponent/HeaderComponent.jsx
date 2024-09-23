109e13f4a58a5a4798149987b99d2a48ca689f60
MariaGargoles <mariagargoles@live.com>
2024-09-23 08:49:49 +0200

    ADD: add nov styles

--- src/components/HeaderComponent/HeaderComponent.jsx
+++ src/components/HeaderComponent/HeaderComponent.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
 
+
 export const HeaderComponent = () => {
 return(
-<header>
-    <h2>Discover and Manage Your Images </h2>
-    <p>Increíble galería de imágenes para descargar.
+<header className="">
+    <div className="">
+    <source src="" autoplay loop></source>
+    </div>
+    <h2 className="">Discover and Manage Your Images </h2>
+    <p className="">Increíble galería de imágenes para descargar.
     Contamos con más de 1 millón de imágenes gracias a Unplash.</p>
-    <NavLink to={"/MyPhotos"}><button>My Photos</button></NavLink>
+    <NavLink className="" to={"/MyPhotos"}><button>My Photos</button></NavLink>
 
 </header>
 
